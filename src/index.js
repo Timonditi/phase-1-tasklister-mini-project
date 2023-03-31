ee2195a43e527ccbad8be9535ad536e9ccfd64c2
Timothy <todongoonditi@gmail.com>
2023-03-31 11:16:33 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form= document.querySelector("#create-task-form");
+  
+  form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let value = event.target.querySelector("#new-task-description").value;
+    console.log(value);
+    createTask(value);
+    form.reset();
+
+
+ 
+   })
 });
+
+
+
+function createTask(task){
+  let taskList=document.querySelector("#tasks");
+  let li=document.createElement("li");
+  li.textContent=task;
+  taskList.appendChild(li);
+ }
